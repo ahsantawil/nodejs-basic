639595187fe9d384c989a00a2520e6b0f961b1a6
ahsantawil <ahsan.tawil07@gmail.com>
2020-07-28 13:21:15 +0700

    modif file app.js

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const fs = require('fs');
+// import file system
+// const fs = require('fs');
 
 
-fs.copyFileSync("text.text","text2.text")
-console.log('File text.text berhasil dicopy')
+// fs.copyFileSync("text.text","text2.text")
+// console.log('File text.text berhasil dicopy')
+
+// const hero = require('superheroes')
+// for(let i = 0; i < 10; i++) {
+//     console.log(hero.random());
+// }
+
+const op = require('./module')
+
+const moduleTitle = op.title;
+const moduleTambah = op.tambah(10,5);
+const moduleKali = op.kali(10,10);
+
+console.log(moduleTitle)
+console.log(moduleTambah)
+console.log(moduleKali)
